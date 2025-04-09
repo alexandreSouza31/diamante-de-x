9542580d73fc80349e72fd1a2ba1abe91f36b070
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-08 21:35:45 -0300

    criará o método de mensagem de erro no número digitado do usuário

--- diamanteDeX.ConsoleApp/Program.cs
+++ diamanteDeX.ConsoleApp/Program.cs
@@ -4,18 +4,25 @@
     {
         //Desenvolver um programa em que, dado um número ímpar como entrada, exibirá um diamante desenhado com a letra 'X'.
 
-        //● O programa deve solicitar ao usuário um número inteiro.[ ]
-        //● O número deve ser ímpar.Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.[ ]
+        //● O programa deve solicitar ao usuário um número inteiro.[X]
+        //● O número deve ser ímpar.Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.[X]
         //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[ ]
         //● A linha central do diamante deve conter o número máximo de Xs.[ ]
         //não utilizar número 1 [ ]
         //não utilizar números quebrados[]
         static void Main(string[] args)
         {
-            while (true)
+            while (true) 
             {
                 ExibirCabecalho();
-            }
+
+                int numeroUsuario=Convert.ToInt32(Console.ReadLine());
+
+                ExibirMensagemErro(numeroUsuario);
+
+                Console.WriteLine("\nPressione Enter para continuar...");
+                Console.ReadLine();
+                }
         }
 
         static void ExibirCabecalho()
@@ -24,6 +31,16 @@
             Console.WriteLine("---------------- Diamante com X ----------------");
             Console.WriteLine("\n------------------------------------------------");
             Console.WriteLine("\nDigite um número inteiro para desenhar o diamante: ");
+        }
+
+        static void ExibirMensagemErro(int numeroUsuario)
+        {
+
+            if (numeroUsuario < 3 || numeroUsuario % 2 == 0)
+            {
+                Console.WriteLine("Número inválido! Digite um ímpar maior que 1");
+
+            }
         }
     }
 }
