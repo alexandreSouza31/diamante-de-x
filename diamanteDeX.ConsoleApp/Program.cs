4cff7c7769a70216318ffeb097d3ac7e733a4fb1
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-08 21:04:49 -0300

    criará o método de exibir cabeçalho

--- diamanteDeX.ConsoleApp/Program.cs
+++ diamanteDeX.ConsoleApp/Program.cs
@@ -2,13 +2,27 @@
 {
     internal class Program
     {
+        //Desenvolver um programa em que, dado um número ímpar como entrada, exibirá um diamante desenhado com a letra 'X'.
+
+        //● O programa deve solicitar ao usuário um número inteiro.[ ]
+        //● O número deve ser ímpar.Caso contrário, o programa deve informar que [ ]
+        //a entrada é inválida e solicitar um novo número.[ ]
+        //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[ ]
+        //● A linha central do diamante deve conter o número máximo de Xs.[ ]
+        //não utilizar número 1 [ ]
+        //não utilizar números quebrados[]
         static void Main(string[] args)
         {
-            while (true) 
-            {
-               Console.WriteLine("Hello, World!");
-            
-            }
+            ExibirCabecalho();
+
+        }
+
+        static void ExibirCabecalho()
+        {
+            Console.WriteLine("---------------- Diamante com X ----------------");
+            Console.Write("\nDigite um número inteiro para desenhar o diamante: ");
+            Console.WriteLine("\n\n------------------------------------------------");
+            Console.ReadLine();
         }
     }
 }
