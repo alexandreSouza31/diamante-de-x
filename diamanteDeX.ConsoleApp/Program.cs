1dc31f6ddc39ed474003f4bf1da9d460d910a6a1
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-10 10:33:19 -0300

    criará o método que desenha a parte superior do diamante

--- diamanteDeX.ConsoleApp/Program.cs
+++ diamanteDeX.ConsoleApp/Program.cs
@@ -8,21 +8,24 @@
         //● O número deve ser ímpar.Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.[X]
         //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[ ]
         //● A linha central do diamante deve conter o número máximo de Xs.[ ]
-        //não utilizar número 1 [ ]
+        //não utilizar número 1 [X]
         //não utilizar números quebrados[]
         static void Main(string[] args)
-        {
-            while (true) 
+        {           
+            while (true)
             {
                 ExibirCabecalho();
 
-                int numeroUsuario=Convert.ToInt32(Console.ReadLine());
+                int numeroUsuario = Convert.ToInt32(Console.ReadLine());
 
                 ExibirMensagemErro(numeroUsuario);
 
                 Console.WriteLine("\nPressione Enter para continuar...");
+                
+                DesenharCimaDiamante(numeroUsuario);
+
                 Console.ReadLine();
-                }
+            }
         }
 
         static void ExibirCabecalho()
@@ -40,6 +43,34 @@
             {
                 Console.WriteLine("Número inválido! Digite um ímpar maior que 1");
 
+            }
+        }
+
+        static void DesenharCimaDiamante(int numeroUsuario)
+        {
+            int qtdEspacos = (numeroUsuario - 1) / 2;
+            int qtdlinhas = qtdEspacos;
+            int qtdDeX = 1;
+
+            string escreveEspaco = " ";
+            string escreveX = "X";
+
+            for(int i = 0; i < qtdlinhas; i++)
+            {
+                for (int espacos=0;espacos<qtdEspacos;espacos++)
+                {
+                    Console.Write(escreveEspaco);
+                }
+
+                for (int x=0;x<qtdDeX;x++)
+                {
+                    Console.Write(escreveX);
+                }
+
+                qtdDeX += 2;
+                qtdEspacos--;
+
+                Console.WriteLine();
             }
         }
     }
