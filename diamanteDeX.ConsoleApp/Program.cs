426d62eedde30eb0cbe7003bc486f88c50bd354a
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-10 14:41:49 -0300

    criará o método que desenha a parte de baixo do diamante

--- diamanteDeX.ConsoleApp/Program.cs
+++ diamanteDeX.ConsoleApp/Program.cs
@@ -6,8 +6,8 @@
 
         //● O programa deve solicitar ao usuário um número inteiro.[X]
         //● O número deve ser ímpar.Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.[X]
-        //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[ ]
-        //● A linha central do diamante deve conter o número máximo de Xs.[ ]
+        //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[X]
+        //● A linha central do diamante deve conter o número máximo de Xs.[X]
         //não utilizar número 1 [X]
         //não utilizar números quebrados[]
         static void Main(string[] args)
@@ -24,6 +24,7 @@
                 
                 DesenharCimaDiamante(numeroUsuario);
                 DesenharMeioDiamante(numeroUsuario);
+                DesenharBaixoDiamante(numeroUsuario);
 
                 Console.ReadLine();
             }
@@ -84,6 +85,32 @@
                 Console.Write(escreveX);
             }
             Console.WriteLine();
+        }
+        static void DesenharBaixoDiamante(int numeroUsuario)
+        {
+            int qtdEspacos = 1;
+            int qtdDeX = numeroUsuario - 2;
+
+            string escreveEspaco = " ";
+            string escreveX = "X";
+
+            for (int i = 0; i < (numeroUsuario - 1) / 2; i++)
+                {
+            for (int espacos = 0; espacos < qtdEspacos; espacos++)
+            {
+                Console.Write(escreveEspaco);
+            }
+
+            for (int x = 0; x < qtdDeX; x++)
+            {
+                Console.Write(escreveX);
+            }
+
+            qtdDeX -= 2;
+            qtdEspacos++;
+
+            Console.WriteLine();
+            }
         }
     }
 }
