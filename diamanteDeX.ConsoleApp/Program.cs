d3628220760f05d0f67585dada2cb2463c2df013
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-10 16:24:58 -0300

    removerá linhas  de comentários desnecessários

--- diamanteDeX.ConsoleApp/Program.cs
+++ diamanteDeX.ConsoleApp/Program.cs
@@ -2,14 +2,6 @@
 {
     internal class Program
     {
-        //Desenvolver um programa em que, dado um número ímpar como entrada, exibirá um diamante desenhado com a letra 'X'.
-
-        //● O programa deve solicitar ao usuário um número inteiro.[X]
-        //● O número deve ser ímpar.Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.[X]
-        //● O número de linhas e colunas deve ser igual ao número ímpar fornecido.[X]
-        //● A linha central do diamante deve conter o número máximo de Xs.[X]
-        //não utilizar número 1 [X]
-        //não utilizar números quebrados[]
         static void Main(string[] args)
         {           
             while (true)
